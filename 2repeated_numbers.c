3ddff432ccf1cc1dbda5cabd831917bbca4b1694
srchittem <srihari.chittem@gmail.com>
2017-02-21 14:55:13 -0800

    initial commit

---
+++ 2repeated_numbers.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <math.h>
+void find_repeating_numbers(int *arr,int size)
+{
+	int i =0;
+	for (i=0;i<size;i++)
+	{
+		if(arr(abs(arr[i])) > 0)
+		{
+			
+		}
+	}
+}
+
+
+
+
+void  main()
+{
+
+	int arr[] = {1,2,3,2,4,4};
+	find_repeating_numbers(arr,sizeof(arr)/sizeof(arr[0]));
+}
